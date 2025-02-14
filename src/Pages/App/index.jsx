d9e06f4fa6e2ba19e2cc62d1a2c27bdabe4d0e98
JoMaCoBa <correabajo@gmail.com>
2025-02-13 20:00:52 -0600

    Project Structure

---
+++ src/Pages/App/index.jsx
@@ -0,0 +1,12 @@
+import '../../App.css'
+
+function App() {
+
+  return (
+    <div className="bg-green-100 h-screen flex items-center justify-center">
+      Hello World 
+    </div>
+  )
+}
+
+export default App
